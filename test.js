96d1e83c784068044ff1a3dcc1f541e3036fa1aa
u-cya334 <yukipoke20@gmail.com>
2021-11-16 00:18:25 +0900

    実験

--- test.js
+++ test.js
@@ -1,8 +1,6 @@
 const $portfolio = document.getElementById('portfolio');
 const $glass = document.getElementsByClassName('glass');
 
-
-
 const adjust = function(){
     $windowW = $(window).width();
     glassW = $glass[0].style.width;
@@ -10,7 +8,6 @@ const adjust = function(){
     glassW = $glass[0].style.width.slice(0,-2);
     $portfolio.style.fontSize = glassW*0.2+"px";
 }
-
 
 window.onload = adjust();
 window.addEventListener("resize",adjust);
