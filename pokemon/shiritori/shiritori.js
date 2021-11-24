a9341783e65f56afd240fd3eb48f79721614900c
u-cya334 <yukipoke20@gmail.com>
2021-11-25 08:18:38 +0900

    勝利バグ修正＆リンク貼り

--- pokemon/shiritori/shiritori.js
+++ pokemon/shiritori/shiritori.js
@@ -16,8 +16,8 @@ let usedList = []; // すでに使われたポケモン一覧
 let userAnswerList = []; // プレイヤーの回答可能一覧
 let userAnsewerLastWord = ""; // プレイヤーの回答可能な最後の文字
 let answerSum = 0; // 答えた回数
-let lose = false;
-let modeSm = true;
+let lose = false; //敗北判定
+let modeSm = true; //スマートフォンかどうか
 const $answerClass = document.getElementsByClassName('answer');
 const replaceWord = ["♂","♀","2","２","Z"];
 const windowWidth = $(window).width();
@@ -87,51 +87,59 @@ $send.addEventListener('click',function(){
 const answerMaking = function(word,last){
     answerSum++;
 
-    //八回目の回答以降背景の大きさを自動調整に
-    if(answerSum===8){
-        console.log('八回とっぱ')
-        $content.style.height = "100%"
-    }
-    i=0;
-    answerList　= [];//答え一覧初期化
-    while(i< allList.length-1){
-        searchName = allList[i].name;
-        if(last==="ー"){
-            last = word.slice(0,-1).slice(-1)
+    console.log("last="+last)
+    if(last==="ン"){
+        console.log("last="+last)
+        gameOver('ンが付いた！キミの負け')
+    }else{
+        //八回目の回答以降背景の大きさを自動調整に
+        if(answerSum===4){
+            console.log('4回とっぱ')
+            $content.style.height = "100%"
         }
-        //小文字→大文字
-        last = last.replace('ァ',"ア").replace("ィ","イ").replace("ゥ","ウ").replace("ェ","エ").replace("ォ","オ").replace("ャ","ヤ").replace("ュ","ユ").replace("ョ","ヨ");
-        if(searchName.slice(0,1)===last&&searchName.slice(-1)!="ン"){
-            //”ン”以外で始まり、最初の文字と最後の文字があっていたら答えの候補に入れる
-            answerList.push(searchName);
+        i=0;
+        answerList　= [];//答え一覧初期化
+        while(i< allList.length-1){
+            searchName = allList[i].name;
+            if(last==="ー"){
+                last = word.slice(0,-1).slice(-1)
+            }
+            //小文字→大文字
+            last = last.replace('ァ',"ア").replace("ィ","イ").replace("ゥ","ウ").replace("ェ","エ").replace("ォ","オ").replace("ャ","ヤ").replace("ュ","ユ").replace("ョ","ヨ");
+            if(searchName.slice(0,1)===last&&searchName.slice(-1)!="ン"){
+                //”ン”以外で始まり、最初の文字と最後の文字があっていたら答えの候補に入れる
+                answerList.push(searchName);
+            }
+            i++;
         }
-        i++;
+        console.log('before'+answerList);
+        console.log('used:'+usedList);
+        //答えの候補からすでに使われた物を除外する
+        answerList = answerList.filter(function(v){
+            return ! usedList.includes(v);
+        })
+        console.log("after:"+answerList);
+        if(answerList.length===0){
+            //回答候補が無ければプレイヤーの勝ち
+            gameOver('キミの勝ち！！');
+        }
+        random = Math.floor(Math.random()*answerList.length); //回答候補からランダムに選択
+        answer = answerList[random];
+        usedList.push(answer);
+        console.log(answer);
+        answerLastWord = answer.slice(-1);
+        console.log("answerLastWord"+answerLastWord);
+        no= noSearch(answer);
+        src = "./img/i_0"+no+".png"; // 画像のパスを取得
+        newMessage("answer",answer,"img",src);
+        makingPlayerAnswer(answer,answerLastWord);        
     }
-    console.log('before'+answerList);
-    console.log('used:'+usedList);
-    //答えの候補からすでに使われた物を除外する
-    answerList = answerList.filter(function(v){
-        return ! usedList.includes(v);
-    })
-    console.log("after:"+answerList);
-    if(answerList.length===0){
-        //回答候補が無ければプレイヤーの勝ち
-        gameOver('キミの勝ち！！');
-    }
-    random = Math.floor(Math.random()*answerList.length); //回答候補からランダムに選択
-    answer = answerList[random];
-    usedList.push(answer);
-    console.log(answer);
-    answerLastWord = answer.slice(-1);
-    console.log(answerLastWord);
-    no= noSearch(answer);
-    src = "./img/i_0"+no+".png"; // 画像のパスを取得
-    newMessage("answer",answer,"img",src);
-    makingPlayerAnswer(answer,answerLastWord);
+
 }
 
 //プレイヤーの回答可能択の検索
 const makingPlayerAnswer = function(word,last){
+    
     i=0;
     userAnswerList　= [];//答え一覧初期化
     while(i< allList.length-1){
@@ -173,7 +181,7 @@ const search=function(word){
     answerLastWord = answerLastWord.replace('ァ',"ア").replace("ィ","イ").replace("ゥ","ウ").replace("ェ","エ").replace("ォ","オ").replace("ャ","ヤ").replace("ュ","ユ").replace("ョ","ヨ");
 
     if(inputWord.slice(0,1)===answerLastWord){
-        console.log('good');
+        console.log('good'+answerLastWord);
         i=0
         exist = false;
         if(replaceWord.includes(word.slice(-1))){
@@ -194,7 +202,8 @@ const search=function(word){
             if(word===searchName){
                 usedList.push(searchName);
                 exist = true;
-                if(answerLastWord==="ン"){
+                if(inputWord==="ン"){
+                    console.log("ンが付いた")
                     gameOver("キミの負け")
                     break
                 }else{
@@ -247,7 +256,16 @@ const newMessage = function(clas,txt,imgclass,src){
         code = '<div class="box"><div class="'+clas+'">'+txt+'</div></div>';
     }
     $content.insertAdjacentHTML('afterbegin',code);
-    $answerClass[0].style.fontSize = "40px";
+    if(modeSm===false){
+        $answerClass[0].style.fontSize = "40px";
+        $(".playerAnswerNo").css("font-size","40px");
+        $(".playerAnswer").css("font-size","40px");
+        $("img").css("width","60px");
+        $("img").css("height","60px");
+        $("imgR").css("width","60px");
+        $("imgR").css("height","60px");
+    }
+    
     
 }
 
